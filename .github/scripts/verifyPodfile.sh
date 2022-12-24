e72e974991d2eebe829a202a446b03238fc05e67
Brayden Williams <brayden@escstrategy.net>
2022-12-23 20:00:32 -0800

    Minor improvements to script (should still fail - packages not changed)

--- .github/scripts/verifyPodfile.sh
+++ .github/scripts/verifyPodfile.sh
@@ -54,8 +54,8 @@ main_branch_specs="$(_get_specs)"
 
 # Perform an array subtraction to determine which pods were removed
 # Store the formatted version here as we won't have the chance after switching back to feature branch
-formatted_subtractions=$(_formatted_pods $(jq -n --args '$ARGS.positional | map(. / "\n" | map(fromjson)) | first - last | .[]' -- \
-"$main_branch_specs" "$feature_branch_specs"))
+formatted_subtractions="$(_formatted_pods "$(jq -n --args '$ARGS.positional | map(. / "\n" | map(fromjson)) | first - last | .[]' -- \
+"$main_branch_specs" "$feature_branch_specs")")"
 
 # Verbosity
 [ "$2" == "-v" ] && echo "Subtractions:" && echo "$formatted_subtractions"
@@ -65,21 +65,23 @@ git reset --quiet HEAD {package,package-lock}.json
 git checkout --quiet -- {package,package-lock}.json
 npm i --silent
 
-# Validate additions and updates to Podfile.lock
+# Validate additions and updates to Podfile.lock (check whether feature branch specs _are not_ in Podfile.lock)
 while read -r SPEC; do
   if ! _in_podlock "$SPEC"; then
     echo -e "${RED}ERROR: Podspec $SPEC not found in Podfile.lock. Did you forget to run \`pod install\`?"
-    exit 1
+    failed=1
   fi
 done <<< "$(_formatted_pods "$feature_branch_specs")"
 
-# Validate deletions
+# Validate deletions (check whether the subtractions _are_ in Podfile.lock)
 while read -r SPEC; do
   if [ ! -z "$SPEC" ] && _in_podlock "$SPEC"; then
     echo -e "${RED} ERROR: Podspec $SPEC was found in Podfile.lock and not part of project. Did you forget to run \`pod install\` after removing the package?"
-    exit 1
+    failed=1
   fi
 done <<< "$formatted_subtractions"
+
+[ "$failed" != "" ] && exit 1
 
 echo -e "${GREEN}Podfile.lock is synced with podspecs."
 exit 0
